e34b60a992610bdcdbd63d2d1e0c26fa37ebd868
naveensnr87 <naveen.snr87@gmail.com>
2018-02-03 09:50:40 -0600

    Add files via upload

---
+++ OrderBO.java
@@ -0,0 +1,11 @@
+package com.naveen.order.bo;
+
+import com.naveen.order.bo.exception.BOException;
+import com.naveen.order.dto.Order;
+
+public interface OrderBO {
+ 
+	boolean placeOrder(Order order) throws BOException;
+	boolean cancelOrder(int id)throws BOException;
+	boolean deleteOrder(int id)throws BOException;
+}
